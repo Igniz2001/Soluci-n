75a7c607dfece32df19a440b65482e2339297565
Igniz <valenciasamuel037@gmail.com>
2022-09-01 22:29:06 -0500

    Corrección
    Me había equivocado pero ahora si es la productoria

--- practica.py
+++ practica.py
@@ -1,11 +1,10 @@
+n = [[1,2,3],[1,2,3],[4,5,6]]
 
-n = [[22,45,67,89],[2,4,6,7],[32,4,5,6],[76,32,76,65]]
-
-def productoria(m,con = 0, cun = 0, pops = 0):
+def productoria(m,con = 0, cun = 0, pops = 1):
     if con == len(m):
         return pops
     else:
-        pops += m[con][cun]
+        pops *= m[con][cun]
         return productoria(m, con+1, cun+1,pops)
 print(productoria(n))
         
